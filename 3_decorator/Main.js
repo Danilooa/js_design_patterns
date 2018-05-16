808cff7408e986fc261da781a7f8468368a65ee0
Danilo de Oliveira Araujo <danilooa@gmail.com>
2018-05-16 07:32:26 -0300

    checked if the log methods were working properly

--- 3_decorator/Main.js
+++ 3_decorator/Main.js
@@ -10,6 +10,12 @@ let legacyInitialData = new LegacyWorkingJustFineInitialData();
 let legacy = new LegacyWorkingJustFine(legacyInitialData);
 legacy.save();
 
+console.log("Logging legacy: ");
+legacy.log();
+
 let decoratedInitialData = new DecoratedInitialData();
 let decorated = new DecoratedLegacy(decoratedInitialData);
 decorated.save();
+
+console.log("Logging decorated: ");
+decorated.log();
